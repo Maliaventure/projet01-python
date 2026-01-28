3cc61f4f23be96b4723c66a8abc5d8b9e16f440a
SPITZLI MALIA <malia.sptzl@localhost.localdomain>
2026-01-28 10:49:05 +0100

    Changer false en true

---
+++ main.py
@@ -0,0 +1,32 @@
+from maqueen import Maqueen
+from microbit import * 
+import utime
+
+# Constantes
+WHITE = 1
+BLACK = 0
+
+robot = Maqueen()
+display.show(Image.HAPPY)
+
+def obstacle ():
+     return (3 < robot.ultrasound_measure() < 10)
+
+while True:
+    if obstacle() == True:
+        robot.motor_left(0)
+        robot.motor_right(0)
+    elif robot.line_left()==BLACK and robot.line_right()==BLACK:
+        robot.motor_left(120)
+        robot.motor_right(120)
+    elif robot.line_left()==WHITE and robot.line_right()==BLACK:
+        robot.motor_left(120)
+        robot.motor_right(30)
+    elif robot.line_left()==BLACK and robot.line_right()==WHITE: 
+        robot.motor_left(30)
+        robot.motor_right(120)
+    else :
+        robot.motor_left(60)
+        robot.motor_right(20)
+        
+        
