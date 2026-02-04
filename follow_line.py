264407ebf52c702615824ad0cd2470131c0458d4
SPITZLI MALIA <malia.sptzl@localhost.localdomain>
2026-02-04 11:41:27 +0100

    musique et reussi suivre ligne et contourner

--- follow_line.py
+++ follow_line.py
@@ -17,6 +17,27 @@ while True:
         robot.motor_left(0)
         robot.motor_right(0)
         sleep(100)
+        robot.motor_right(30,1)
+        robot.motor_left(30,1)
+        sleep(700)
+        robot.motor_right(30)
+        robot.motor_left(0)
+        sleep(800)
+        robot.motor_right(30)
+        robot.motor_left(30)
+        sleep(2500)
+        robot.motor_left(30)
+        robot.motor_right(0)
+        sleep(2000)
+        while robot.line_left()==WHITE and robot.line_right()==WHITE:
+            robot.motor_right(30)
+            robot.motor_left(30)
+        if robot.line_left()==BLACK or robot.line_right()==BLACK :
+            robot.motor_right(30)
+            robot.motor_left(0)
+            sleep(500)
+        
+        
     elif robot.line_left()==BLACK and robot.line_right()==BLACK:
         robot.motor_left(100)
         robot.motor_right(100)
@@ -29,3 +50,13 @@ while True:
     else :
         robot.motor_left(60)
         robot.motor_right(20)
+        
+        
+        
+        
+#import music
+#import utime
+#for k in range (4):
+ #   music.play(["E5:2"])
+  #  utime.sleep(0.75)
+    
