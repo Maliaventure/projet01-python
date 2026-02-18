89163a32b516f55b4dc2f38eae1cfec9adb0dff4
SPITZLI MALIA <malia.sptzl@localhost.localdomain>
2026-02-18 11:17:44 +0100

    c'est le code de la telecommande

--- telecommande_MB0.py
+++ telecommande_MB0.py
@@ -1,26 +1,45 @@
+''' Auteur(s) : Lea W., Malia S.
+    Contact : Malia.sptzl@eduge.ch, lea.wttk@eduge.ch
+    License : "CC-BY-NC-SA"
+    Date : 18 février 2026
+    Version : 1.0
+    Description : Code de la télécommande
+'''
+
+
+
+
+
+
+
 from microbit import *
 
 import radio
 
 radio.on()
 
-radio.config(group=7)
+display.off()
+display.clear()
 
+radio.config(group=7)
+Z = pin6
 X = pin1
 Y = pin0
 message = ''
 while True:
-    if Y.read_analog() > 530:
+    if Y.read_analog()  > 530 and 450 < X.read_analog() < 550 :
         message = 'avance'
 
-    elif Y.read_analog() < 500:
+    elif Y.read_analog() < 500 and 450< X.read_analog() < 550:
         message = 'recule'
     
-    elif X.read_analog() > 530:
+    elif X.read_analog() > 550 and 450< Y.read_analog()< 550:
         message = 'droite'
     
-    elif X.read_analog() < 500:
+    elif X.read_analog() < 500 and 450 < Y.read_analog() < 550:
         message = 'gauche'
+    elif Z.read_digital() == 1 :
+        message = 'arret'
     else:
         message = 'stop'
 
@@ -28,3 +47,6 @@ while True:
     if message is not 'stop':
         radio.send(message)
         print(message)
+        print("Y=",Y.read_analog())
+        print("X=",X.read_analog())
+        print("Z=",Z.read_digital())
